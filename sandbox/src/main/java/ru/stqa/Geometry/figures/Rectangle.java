9a95cf5e3dd6840e482670c04a9fd185a02c25f2
DSelivanova <dselivanova@it.ru>
2025-02-25 20:00:00 +0100

    создание пакетов для геометрии, прямоугольного треугольника и прямоугольника

---
+++ sandbox/src/main/java/ru/stqa/Geometry/figures/Rectangle.java
@@ -0,0 +1,12 @@
+package ru.stqa.Geometry.figures;
+
+public class Rectangle {
+    public static void printRectangleArea(double a, double b) {
+        var text = String.format("Площадь прямоугольника со сторонами %f и %f равна %f", a, b, rectangleArea(a,b))
+        System.out.println(text);
+    }
+
+    private static double rectangleArea(double a, double b) {
+        return a*b;
+    }
+}
---
+++ sandbox/src/main/java/ru/stqa/Geometry/geometry.java
@@ -0,0 +1,18 @@
+package ru.stqa.Geometry;
+
+import ru.stqa.Geometry.figures.Rectangle;
+import ru.stqa.Geometry.figures.Square;
+
+public class geometry {
+    public static void main(String[] args) {
+        Square.printSquareArea(6.8);
+        Square.printSquareArea(8.8);
+        Square.printSquareArea(6.2);
+
+        Rectangle.printRectangleArea (3.0, 5.0);
+        Rectangle.printRectangleArea (4.0, 5.0);
+        Rectangle.printRectangleArea (7.0, 9.0);
+
+    }
+
+}
