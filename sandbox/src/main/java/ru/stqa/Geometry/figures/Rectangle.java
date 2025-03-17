779766059e0a928c56abd160a92cc735cf2636f9
DSelivanova <dselivanova@it.ru>
2025-03-17 16:45:38 +0100

    equals на примере квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/Geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/Geometry/figures/Rectangle.java
@@ -1,6 +1,20 @@
 package ru.stqa.Geometry.figures;
 
+import java.util.Objects;
+
 public record Rectangle (double a, double b) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return (Double.compare(a, rectangle.a) == 0 && Double.compare(b, rectangle.b) == 0)|| (Double.compare(a, rectangle.b) == 0 && Double.compare(b, rectangle.a) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
 
     public Rectangle{
         if (a < 0 || b < 0 ){
